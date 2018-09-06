29a3640022e2d56ec75edafaf4f9c8d6227b2c67
vivian-v <37598050+vivian-v@users.noreply.github.com>
2018-09-05 23:58:18 -0500

    Add files via upload
    finished player

--- src/Player.java
+++ src/Player.java
@@ -9,8 +9,11 @@ public class Player {
 	int Asiaheld;
 	int AustraliaHeld;
 
+	
 	private String playerName;
 	private int numTroops;
+    private HashMap<String, Country> ownedCountries;
+    private ArrayList<Card> cardHeld;
     int playerID;
     
 	public Player (String name, int num)
@@ -24,25 +27,120 @@ public class Player {
 		this.AfricaHeld = 0;
 		this.Asiaheld = 0;
 		this.AustraliaHeld = 0;
+		cardHeld = new ArrayList<Card>();
+		ownedCountries = new HashMap<String, Country>();
+		
+
 	}
 	
 	public void setPlayerID(int x)
 	{
 		this.playerID = x;
 	}
-	
+	public int getTotalNumCountries()
+	{
+		return ownedCountries.size();
+	}
 	public int getPlayerID()
 	{
 		return this.playerID;
 	}
-	
 	public String getPlayerName()
 	{
 		return this.playerName;
 	}
-	
 	public int getPlayerTroops()
 	{
 		return numTroops;
-	}	
+	}
+	public void addTroops(int x)
+	{
+		numTroops += x;
+	}
+	public void takeCountry(Country country)
+	{		
+		ownedCountries.put(country.getCountryName(), country);
+		AddRegionHeld(country);
+	}
+	
+	public void loseCountry(Country country)
+	{
+		ownedCountries.remove(country.getCountryName());
+		MinusRegionHeld(country);
+	}
+	public void MinusRegionHeld(Country country)
+	{
+		String tempStr = country.getContinent();
+		if (tempStr.equals("North America"))
+			this.NorthAmericaHeld--;
+		else if (tempStr.equals("South America"))
+			this.SouthAmericaHeld--;
+		else if (tempStr.equals("Europe"))
+			this.EuropeHeld--;
+		else if (tempStr.equals("Africa"))
+			this.AfricaHeld--;
+		else if (tempStr.equals("Asia"))
+			this.Asiaheld--;
+		else if (tempStr.equals("Australia"))
+			this.AustraliaHeld--;
+	}
+	public void AddRegionHeld(Country country)
+	{
+		String tempStr = country.getContinent();
+		if (tempStr.equals("North America"))
+			this.NorthAmericaHeld++;
+		else if (tempStr.equals("South America"))
+			this.SouthAmericaHeld++;
+		else if (tempStr.equals("Europe"))
+			this.EuropeHeld++;
+		else if (tempStr.equals("Africa"))
+			this.AfricaHeld++;
+		else if (tempStr.equals("Asia"))
+			this.Asiaheld++;
+		else if (tempStr.equals("Australia"))
+			this.AustraliaHeld++;
+	}
+	public int getNumNorthAmerica()
+	{
+		return this.NorthAmericaHeld;
+	}
+	public int getNumSouthAmerica()
+	{
+		return this.SouthAmericaHeld;
+	}
+	public int getNumEurope()
+	{
+		return this.EuropeHeld;
+	}
+	public int getNumAfrica()
+	{
+		return this.AfricaHeld;
+	}
+	public int getNumAsia()
+	{
+		return this.Asiaheld;
+	}
+	public int getNumAustralia()
+	{
+		return this.AustraliaHeld;
+	}
+	public void addCard(Card card)
+	{
+		cardHeld.add(card);
+	}
+	public void putOneAmry()
+	{
+		numTroops -= 1;
+	}
+	
+	public ArrayList<Country> showMyCountries()
+	{
+		return new ArrayList<Country>(ownedCountries.values());
+		
+	}
+	public Country getCountry(String s)
+	{
+		return ownedCountries.get(s);
+	}
+	
 }
