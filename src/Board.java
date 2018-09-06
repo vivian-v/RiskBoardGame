8f08e928b38275cd4fa11f868efb750cd5e3731d
vivian-v <37598050+vivian-v@users.noreply.github.com>
2018-09-06 00:21:22 -0500

    update
    displayMenu, playerInfo, gamestart

--- src/Board.java
+++ src/Board.java
@@ -29,7 +29,6 @@ public class Board {
 		
 		
 	}
-
 	public void setupCountryIndex()
 	{
 		countriesIdx = new ArrayList<String>();
@@ -39,7 +38,31 @@ public class Board {
 			countriesIdx.add(countries.get(i).getCountryName());
 		}
 	}
-
+	public void GameStart()
+	{
+		Boolean end = false;
+		show.gameStart();	
+		
+		displayMapInfo();
+		ArmyPlacement();
+		int idx = 0;
+		for (int i = 0; i < 1; i++)
+		{
+//			Fortify(players[idx].getPlayerID());
+			displayMapInfo();
+			displayPlayerInfo(players[idx].getPlayerID());
+			Reinforce(players[idx].getPlayerID());
+			Attack(players[idx].getPlayerID());
+			 
+		
+		}
+		
+		//System.out.println(countries.get(getCountryIndex("China")).getOwnerName());
+		
+		displayMapInfo();
+//		displayPlayerInfo(0);
+		
+	}
 
 	public int getCountryIndex(String c)
 	{
@@ -58,5 +81,47 @@ public class Board {
 				return i;
 		}
 		return -1;
-  }
+	}
+	
+
+	public void displayPlayerInfo(int n)
+	{
+		show.spaceFormat();
+
+		System.out.println(players[n].getPlayerName() + "'s Information\n");
+		System.out.format(format, "Index", "Region" ,"Country Name", "Land Owner", "Num of Army");
+		playerCountries = players[n].showMyCountries();
+
+		for (int i = 0; i < playerCountries.size(); i++)
+		{
+			   System.out.format(format, i+1, playerCountries.get(i).getContinent(),playerCountries.get(i).getCountryName(), playerCountries.get(i).getOwnerName(), playerCountries.get(i).getNumOfArmy());
+		}
+		
+		System.out.println("\n=Statistic=");
+		System.out.println("Number of Troops : " + players[n].getPlayerTroops());
+		
+		System.out.println("1. North America : " + players[n].getNumNorthAmerica());
+		System.out.println("2. South America : " + players[n].getNumSouthAmerica());
+		System.out.println("3. Europe        : " + players[n].getNumEurope());
+		System.out.println("4. Africa        : " + players[n].getNumAfrica());
+		System.out.println("5. Asia          : " + players[n].getNumAsia());
+		System.out.println("6. Australia     : " + players[n].getNumAustralia());
+
+		
+		
+	}
+	public void displayMenu()
+	{
+		
+	}
+
+	public void displayMapInfo()
+	{
+		System.out.format(format, "Index", "Region" ,"Country Name", "Land Owner", "Num of Army");
+		for (int i = 0; i < countries.size(); i++)
+		{
+			   System.out.format(format, i+1, countries.get(i).getContinent(),countries.get(i).getCountryName(), countries.get(i).getOwnerName(), countries.get(i).getNumOfArmy());
+		}
+	
+	}
 }
