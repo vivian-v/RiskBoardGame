dcee83f4b7594021a2d8ac850a72533307d4b897
changjin86 <blcj777@hotmail.com>
2018-09-06 00:12:41 -0500

    add Board class

---
+++ src/Board.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Board {
+	int countryIndex;
+	int dir= 0;
+	private int numOfPlayers; 
+	private Player[] players;
+	private ArrayList<Country> countries;
+	private ArrayList<Country> playerCountries;
+	private ArrayList<String> countriesIdx;
+	
+	private Dice dice;
+	private Deck deck;
+	private Displayer show;
+	int step = 0;
+	String format = "%1$-5s | %2$-20s | %3$-24s | %4$-15s | %5$-11s | \n";
+	
+	public Board(Player[] p, ArrayList<Country> c, Deck dk, Dice d)
+	{
+		this.countryIndex = 0;
+		this.players = p;
+		this.countries = c;
+		this.deck = dk;
+		this.dice = d;
+		this.show = new Displayer(c);
+		numOfPlayers = p.length;
+		setupCountryIndex();
+		
+		
+	}
+	
+}
