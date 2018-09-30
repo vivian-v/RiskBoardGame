db1717b45262a53df7a37570ec1e127e46b39494
DuongNguyen7691 <duongntt91dn@gmail.com>
2018-09-29 22:09:38 -0500

    upload test cases

---
+++ RiskBoardGame/src/test/java/TestDice.java
@@ -0,0 +1,33 @@
+package demo3;
+
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+
+public class TestDice extends TestCase {
+
+	Dice test = new Dice();
+
+
+    @Test
+    public void testRoll() {
+    	int[] testNum = test.roll(3);
+    	
+    	assertTrue(testNum[0] > 0 && testNum[0] < 7);
+    	assertTrue(testNum[1] > 0 && testNum[1] < 7);
+    	assertTrue(testNum[2] > 0 && testNum[2] < 7);
+
+    	assertEquals(2, test.roll(2).length);
+    	assertEquals(3, test.roll(3).length);
+    	
+
+    }
+    @Test
+    public void testForone() {
+    	assertTrue(test.rollForOne() > 0 && test.rollForOne() < 7);
+    }
+
+	
+
+}
