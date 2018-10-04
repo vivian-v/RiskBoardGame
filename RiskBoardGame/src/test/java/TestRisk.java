3e70edd103e96e3ffec04251a0f0a8f70c5028d7
vivian-v <37598050+vivian-v@users.noreply.github.com>
2018-10-04 02:32:24 -0500

    Create TestRisk.java

---
+++ RiskBoardGame/src/test/java/TestRisk.java
@@ -0,0 +1,21 @@
+
+import java.io.IOException;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+
+
+public class TestRisk extends TestCase {
+
+	Risk risk = new Risk();
+	
+    @Test
+    public void testMain() throws IOException {
+    	String[] mainArgs= {"=^-^= Risk Board Game"};
+    	
+    	
+    }
+ 
+	
+}
