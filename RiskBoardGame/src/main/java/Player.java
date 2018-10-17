8ea9dc7ad61c358891139f8b17ef73b16d284070
VincentChungHeiPoon <35947681+VincentChungHeiPoon@users.noreply.github.com>
2018-10-17 15:24:09 -0500

    Update Player.java

--- RiskBoardGame/src/main/java/Player.java
+++ RiskBoardGame/src/main/java/Player.java
@@ -1,8 +1,10 @@
-package demo3;
+package riskboardgame;
 
 import java.util.ArrayList;
 
-public class Player  {
+
+public class Player implements Observer, Transaction {
+	private ArrayList<Observer> warObservers = new ArrayList<Observer>();
 	
 	private int numOfTroops;
     private int [] countriesOwnedByRegions;
@@ -13,6 +15,11 @@ public class Player  {
 	private ArrayList<Country> ownedCountries;
 	private ArrayList<Card> cards;
 	private int tradeSetIndex;
+	private int credit;
+	private Transaction account;
+	private int numConquered;
+	private boolean dead;
+	
 	public Player(String s, int n)	{
 		this.playerName = s;
 		this.numOfTroops = n;
@@ -20,6 +27,25 @@ public class Player  {
 		this.cards = new ArrayList<Card>();
 		this.countriesOwnedByRegions = new int[6];
 		this.tradeSetIndex = 1;
+		this.account = new Proxy(20);
+		this.credit = 20;
+		this.numConquered = 0;
+		this.dead = false;
+	}
+	public Player(int credit2) {
+		this.credit = credit2;
+	}
+	public void killPlayer()
+	{
+		this.dead = true;
+	}
+	public int getNumConquered()
+	{
+		return this.numConquered;
+	}
+	public void increaseNumConquered()
+	{
+		this.numConquered++;
 	}
 	public int getTradeSetIndex()
 	{
@@ -33,7 +59,11 @@ public class Player  {
 	{
 		return this.ownedCountries.size() < 1 ? true : false;
 	}
-	
+	public int buyccc()
+	{
+		account.buyCards();
+		return this.credit;
+	}
 	public String getPlayerName() {
 		return this.playerName;
 	}
@@ -47,8 +77,11 @@ public class Player  {
 		this.playerName = n;
 	}
 
-	public void setNumOfTroops(int n) {
+	public void addNumOfTroops(int n) {
 		this.numOfTroops += n;
+	}
+	public void loseNumOfTroops(int n) {
+		this.numOfTroops -= n;
 	}
 	
 	public void takeCountry(Country c) {
@@ -90,6 +123,37 @@ public class Player  {
 				break;
 			}
 		}
+	}
+//	public void setCredit(int n)
+//	{
+//		this.credit = n;
+//	}
+	@Override
+	public void update() {
+		System.out.println(this.playerName + " being attacked");
+		
+	}
+	@Override
+	public int buyCards() {
+		System.out.print("Remaining Credit After Buy a Card : ");
+		return this.credit-= 1;
+	}
+	@Override
+	public int buyUndoActions() {
+		System.out.print("Remaining Credit After Buy an Undo Action : ");
+		return this.credit-= 5;
+	}
+	@Override
+	public int transferCredit() {
+		System.out.print("Transferable Credit : ");
+		return this.credit;
+		
+	}
+	
+	@Override
+	public int getCredit()
+	{
+		return this.credit;
 	}
 
 }
