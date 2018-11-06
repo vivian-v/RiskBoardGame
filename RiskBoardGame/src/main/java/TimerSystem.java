27388ed1a3f8e37e1a04fcd4e0ccd6f3c10488bf
changjin86 <blcj777@hotmail.com>
2018-11-05 20:15:00 -0600

    Uploaded TimerSystem class

---
+++ RiskBoardGame/src/main/java/TimerSystem.java
@@ -0,0 +1,18 @@
+package riskboardgame;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerSystem {
+//	 Timer timer = new Timer("One Time Timer Test");
+//     TimerTask task = new TimerTask() {
+//         @Override
+//         public void run() {
+//             System.out.println("Execution time is :" + System.currentTimeMillis() + " Name of the thread is :" + Thread.currentThread().getName());
+//         }
+//     };
+//
+//     timer.schedule(task, 1000L);
+//     
+//     
+}
