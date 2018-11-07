3e7fb47a1d541aff9c27df17a0785fc74f990882
vivian-v <37598050+vivian-v@users.noreply.github.com>
2018-11-07 12:55:01 -0600

    Update Risk.java

--- RiskBoardGame/src/main/java/Risk.java
+++ RiskBoardGame/src/main/java/Risk.java
@@ -3,10 +3,29 @@ package riskboardgame;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.apache.http.HttpEntity;
+
 public class Risk {
 
 	public static void main(String[] args) {
 		
+		ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Board());
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+		
+        
+        
+        
+        
+        
 		Setup setup = new Setup();
 		try {
 			Board riskBoard = new Board(setup.LoadMap(), setup.setupPlayer(2));
