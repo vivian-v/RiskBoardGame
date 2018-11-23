65e50e00e8ae00cb86b29ae70fa04b37a3923bdc
vivian-v <37598050+vivian-v@users.noreply.github.com>
2018-11-22 20:38:42 -0600

    Update MyBot.java

--- RiskBoardGame/src/main/java/MyBot.java
+++ RiskBoardGame/src/main/java/MyBot.java
@@ -12,78 +12,57 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class MyBot extends  TelegramLongPollingBot
 {
-	int gameId;
-	int test;
-	//SendMessage message = new SendMessage();
+//-281194447
 	
-	@Override
-    public void onUpdateReceived(Update update) {
+	 @Override
+	    public void onUpdateReceived(Update update) {
 
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            // Set variables
-            String user_first_name = update.getMessage().getChat().getFirstName();
-            
-            String user_last_name = update.getMessage().getChat().getLastName();
-            
-            String user_username = update.getMessage().getChat().getUserName();
-            
-            long user_id = update.getMessage().getChat().getId();
-            
-            String message_text = update.getMessage().getText();
-            
-            //long chat_id = -271143153;
-            long chat_id = update.getMessage().getChatId();
-            //String answer = "why it doesn't work...";
-            String answer = message_text;
-            
-            
-            SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(answer);
-            
-            
-            log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
-           
+	        // We check if the update has a message and the message has text
+	        if (update.hasMessage() && update.getMessage().hasText()) {
+	            // Set variables
+	            String message_text = update.getMessage().getText();
+	            long chat_id = update.getMessage().getChatId();
+	            //chat_id = -281194447;
+	            
+	            System.out.println(update.getChannelPost());
 
-//            try {
-//                execute(message); // Sending our message object to user
-//            } catch (TelegramApiException e) {
-//                e.printStackTrace();
-//            }
-//            
-//            
-            
-        }
-    }
-	   
-	   public boolean log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
-	        System.out.println("\n ----------------------------");
-//	        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//	        Date date = new Date();
-//	        System.out.println(dateFormat.format(date));
-	        System.out.println("Message from " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
-//	        System.out.println("Bot answer: \n Text - " + bot_answer);
-	        
-	        return true;
+	            SendMessage message = new SendMessage() // Create a message object object
+	                .setChatId(chat_id)
+	                .setText(message_text);
+	            
+	            
+	            try {
+	                execute(message); // Sending our message object to user
+	            } catch (TelegramApiException e) {
+	                e.printStackTrace();
+	            }
+	            
+	            chat_id = -281194447;
+
+	            message = new SendMessage() // Create a message object object
+	                .setChatId(chat_id)
+	                .setText(message_text);
+	            
+	            
+	            try {
+	                execute(message); // Sending our message object to user
+	            } catch (TelegramApiException e) {
+	                e.printStackTrace();
+	            }
+	        }
 	    }
-	   
-	   public int DisplayGameID(int id)
-	   {
-		   this.gameId = id;
-		   return this.gameId;
-	   }
+
 	   
 	   
-	   
-	    @Override
+	   @Override
 	    public String getBotUsername() {
-	        return "RiskGameBot";
+	        return "MeowChatBot";
 	    }
- 
+
 	    @Override
 	    public String getBotToken() {
-	        return "718366234:AAHZ64pich1qeITo4J2S8CmauBCfPSqCkQY";
+	        //return "718366234:AAHZ64pich1qeITo4J2S8CmauBCfPSqCkQY";
+	        return "704843018:AAGMDCsXfqVmOF_umFGSdZgJNAMiFyVr0d4";
 	    }
 	    
 }
