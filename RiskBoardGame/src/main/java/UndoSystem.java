642b31d6f28565bf17b9cf2e4c853e6de8e9f6ae
DuongNguyen7691 <duongntt91dn@gmail.com>
2018-11-23 13:55:13 -0600

    Update UndoSystem.java

--- RiskBoardGame/src/main/java/UndoSystem.java
+++ RiskBoardGame/src/main/java/UndoSystem.java
@@ -1,15 +1,13 @@
 package riskboardgame;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class UndoSystem {
-	Stack<History> stack = new Stack();
+	Stack<History> stack = new Stack<History>();
 	
 	public History undo()
 	{
 		History oldHistory = stack.pop();
-		//("Player " + oldHistory.getPrevPlayers().get(oldHistory.getPrevPlayerIndex()).getPlayerName() + " undo " + oldHistory.getActionStatus() + "\n");
 		return oldHistory;
 		
 	}
