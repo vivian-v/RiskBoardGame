69dd2167d748db193b2b48259473a56226884b9b
vivian-v <37598050+vivian-v@users.noreply.github.com>
2018-11-27 20:19:00 -0600

    Add files via upload

---
+++ RiskBoardGame/src/test/java/TestWarObserver.java
@@ -0,0 +1,39 @@
+
+
+
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import riskboardgame.Player;
+import riskboardgame.WarObserver;
+
+public class TestWarObserver extends TestCase {
+
+	WarObserver warObserver = new WarObserver();
+	Player p1 = new Player("Vivian", 123, 1);
+	Player p2 = new Player("Vincent", 123, 1);
+
+    @Test
+    public void testAddObserver() {
+    	warObserver.addObserver(p1);
+		warObserver.addObserver(p2);
+		assertNotNull(warObserver);
+//		warObserver.notifyWarObservers();
+    }
+    @Test
+    public void testRemoveObserver() {
+    	warObserver.addObserver(p1);
+		warObserver.addObserver(p2);
+		warObserver.removeObserver(p2);
+    	assertEquals(1, warObserver.notifyWarObservers());
+    }
+    @Test
+    public void testNotifyWarObservers() {
+      	warObserver.addObserver(p1);
+    	warObserver.addObserver(p2);
+    	assertEquals(2, warObserver.notifyWarObservers());
+    }
+
+	
+
+}
