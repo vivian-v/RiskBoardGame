80c5feb2be3539779125db1842aa8543827d1dcc
changjin86 <blcj777@hotmail.com>
2018-11-30 16:25:09 -0600

    Update TestRisk.java

--- RiskBoardGame/src/test/java/TestRisk.java
+++ RiskBoardGame/src/test/java/TestRisk.java
@@ -1,41 +1,23 @@
-package riskboardgame;
+
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.apache.http.HttpEntity;
+import org.junit.Test;
+import junit.framework.TestCase;
+import riskboardgame.Risk;
 
-public class Risk {
 
-	public static void main(String[] args) {
-		Setup setup = new Setup();
 
-		ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(new Board(setup.LoadMap(), setup.LoadDeck()));
+public class TestRisk extends TestCase {
 
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-		
-		
-//		new Board();
-
-//		try {
-//			new Board();
-//
-//			new Board(setup.LoadMap(), setup.setupPlayer(2));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
-	}
-
+	Risk risk = new Risk();
 	
-
-
+    @Test
+    public void testMain() throws IOException {
+    	String[] mainArgs= {"=^-^= Risk Board Game"};
+    	
+    	
+    }
+ 
+	
 }
