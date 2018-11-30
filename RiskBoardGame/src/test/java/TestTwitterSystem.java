eebeb195113561a437ce1d6e077db038f36e5227
DuongNguyen7691 <duongntt91dn@gmail.com>
2018-11-30 15:44:11 -0600

    Add files via upload

---
+++ RiskBoardGame/src/test/java/TestTwitterSystem.java
@@ -0,0 +1,22 @@
+
+
+
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import riskboardgame.TwitterSystem;
+
+
+public class TestTwitterSystem extends TestCase {
+
+	TwitterSystem tweet = new TwitterSystem();
+
+
+    @Test
+    public void testGetKeysNTokens() {
+    	assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", tweet.getKeysNTokens("secret_MeowCat.properties").get("oauth.consumerKey"));
+    }
+
+	
+
+}
